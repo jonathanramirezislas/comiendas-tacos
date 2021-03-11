5836bf4f6731ff41ba5154c00827ac1fc7a2b275
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-03-11 14:45:20 -0600

    set up firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import { Routes, Route } from 'react-router';
 import './styles/main.css';
+import firebase, { FirebaseContext } from './firebase'; // ./firebase/index 
 
-import Sidebar  from './components/ui/Sidebar';
-import Ordenes  from './components/pages/Ordenes';
+import Sidebar from './components/ui/Sidebar';
+import Ordenes from './components/pages/Ordenes';
 import Menu from './components/pages/Menu';
 import NuevoPlatillo from './components/pages/NuevoPlatillo';
 
 const App = () => {
 	return (
-		<div className="min-h-screen md:flex">
-			<Sidebar />
-			<div className="p-6 md:w-2/5 xl:w-4/5">
-				<Routes>
-					<Route path="/" element={<Ordenes />} />
-					<Route path="/menu" element={<Menu />} />
-					<Route path="/nuevo-platillo" element={<NuevoPlatillo />} />
-				</Routes>
+		<FirebaseContext.Provider
+			value={{
+				firebase,
+			}}
+		>
+			<div className="min-h-screen md:flex">
+				<Sidebar />
+				<div className="p-6 md:w-2/5 xl:w-4/5">
+					<Routes>
+						<Route path="/" element={<Ordenes />} />
+						<Route path="/menu" element={<Menu />} />
+						<Route path="/nuevo-platillo" element={<NuevoPlatillo />} />
+					</Routes>
+				</div>
 			</div>
-		</div>
+		</FirebaseContext.Provider>
 	);
 };
 export default App;
---
+++ src/firebase/index.js
@@ -0,0 +1,7 @@
+import firebase from './firebase';
+import FirebaseContext from './context';
+
+//we export the config
+
+export { FirebaseContext }
+export default firebase;
