a3249e173c12700f70f9625219fc28459a5551b1
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-03-14 17:54:32 -0600

    obtener platillols en tiempo real de fb

--- src/components/pages/Menu.js
+++ src/components/pages/Menu.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { FirebaseContext } from '../../firebase';
 
 const Menu = () => {
+
+	// definir el state para los platillos
+	const [platillos, setPlatillos] = useState([]);
+
+	const { firebase } = useContext(FirebaseContext);
+
+	// consultar la base de datos al cargar
+	useEffect(() => {
+		const obtenerPlatillos = () => {
+			//real time
+			firebase.db.collection('productos').onSnapshot(manejarSnapshot);
+		}
+		obtenerPlatillos();
+	}, []);
+
+	// Snapshot nos permite utilizar la base de datos en tiempo real de firestore
+	function manejarSnapshot(snapshot) {
+		const platillos = snapshot.docs.map(doc => {
+			return {
+				id: doc.id,
+				...doc.data() //contenido del platillo
+			}
+		});// almacenar los resultados en el state
+		setPlatillos(platillos);
+	}
+
 	return (
 		<>
 			<h1 className="mb-4 text-3xl font-light">Menu</h1>
@@ -11,7 +38,7 @@ const Menu = () => {
 			>
 				Agregar Platillo
 			</Link>
-            
+
 		</>
 	);
 };
