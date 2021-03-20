9db44a12059178ad43a65ed496f87f71a40addd1
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-03-20 14:24:48 -0600

    ordenes

--- src/components/pages/Ordenes.js
+++ src/components/pages/Ordenes.js
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react';
+import { FirebaseContext } from '../../firebase';
+import Orden from '../ui/Orden';
 
 const Ordenes = () => {
-  return (
-    <div>
-          <h1 className="ml-80 mb-4 text-3xl font-light">Hi!</h1>  
-    </div>
-  )
+
+    // context con las operaciones de firebase
+    const { firebase } = useContext(FirebaseContext);
+
+    // state con las ordenes
+    const [ordenes, setOrdenes] = useState([]);
+
+    useEffect(() => {
+        const obtenerOrdenes = () => {
+            firebase.db.collection('ordenes').where('completado', "==", false).onSnapshot(manejarSnapshot);
+        }
+        obtenerOrdenes();
+    }, []);
+
+    function manejarSnapshot(snapshot) {
+        const ordenes = snapshot.docs.map(doc => {
+            return {
+                id: doc.id,
+                ...doc.data()
+            }
+        });
+
+        setOrdenes(ordenes);
+    } 
+
+    return ( 
+        <div className="ml-80">
+            <h1 className="text-3xl font-light mb-4">Ordenes</h1>
+
+            <div className="sm:flex sm:flex-wrap -mx-3">
+                {ordenes.map(orden => (
+                    <Orden
+                        key={orden.id}
+                        orden={orden}
+                    />
+                ))}
+            </div>
+        </div>
+     );
 }
-
-export default Ordenes
-
-
+ 
+export default Ordenes;
